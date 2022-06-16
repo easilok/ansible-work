b9fcf182ac98b3c8bf1019d89c2f64458d7496d0
Luis Pereira <luispereira.tkd@gmail.com>
2022-06-16 16:59:30 +0100

    Fixed play script tags arguments

--- play_the_book.sh
+++ play_the_book.sh
@@ -3,14 +3,12 @@
 if [ "$#" -gt 0 ]; then
   # run with tags
   # the first tag will be duplicated, because the script its easier
-  TAGS="[$1"
-  for TAG in "$@"; do
-    TAGS=$TAGS", $TAG"
+  TAGS="$1"
+  for TAG in "${@:2}"; do
+    TAGS=$TAGS",$TAG"
   done
 
-  TAGS=$TAGS"]"
-
-  ansible-playbook -K --tags $TAGS run.yml
+  echo "ansible-playbook -K --tags $TAGS run.yml"
 
 else
   ansible-playbook -K run.yml
