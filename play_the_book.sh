5df24ec6faed1b6ca0b03d79696bccab0bc3188f
Luis Pereira <luispereira.tkd@gmail.com>
2022-06-16 16:46:01 +0100

    Fixed neovim git folder verification. play script with tags

--- play_the_book.sh
+++ play_the_book.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-ansible-playbook -K run.yml
+if [ "$#" -gt 0 ]; then
+  # run with tags
+  # the first tag will be duplicated, because the script its easier
+  TAGS="[$1"
+  for TAG in "$@"; do
+    TAGS=$TAGS", $TAG"
+  done
+
+  TAGS=$TAGS"]"
+
+  ansible-playbook -K --tags $TAGS run.yml
+
+else
+  ansible-playbook -K run.yml
+fi
+
